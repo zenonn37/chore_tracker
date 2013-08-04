d25ab9db71aa5f0946496a5df690b756838ae1bd
chris crowd <zent29@gmail.com>
2013-08-04 15:14:56 -0400

    working on controllers

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,13 +1,40 @@
 class ChoresController < ApplicationController
   def index
+
+  	render('list')
   end
 
   def list
+  	@chores = Chore.all
   end
 
   def new
+  	@chores = Chore.new
+  end
+
+  def create
+  	@chores = Chore.new(params[:chores])
+
+  		if @chores.save
+
+  		else
+
+
+  		end
   end
 
   def show
+  end
+
+  def destroy
+  end
+
+  def update
+
+  	@chores = Chore.find(params[:id])
+  	if @chores.update_attributes(params[:chores])
+  	else
+  		redirect_to list_path
+  	end
   end
 end
