27629d57e07395186254907378519a9cb62802b9
chris crowd <zent29@gmail.com>
2013-08-17 09:23:28 -0400

    finished crud for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,34 @@ class UsersController < ApplicationController
 
   end
 
+
+def show
+  @user = User.find(params[:id])
+end
+
+def edit
+  @user = User.find(params[:id])
+  
+end
+
+def destroy
+  @user = User.find(params[:id])
+  @user.destroy
+  redirect_to users_path
+  end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+     redirect_to users_path
+   else
+    redirect_to edit_user_path
+  end
+
+  end
+
+
+
   private
 
   def user_params
